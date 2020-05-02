1647ee4fe8cb5e2b7fe3dcba4e887366dc6867a3
sidmonta <sidmonta.15@gmail.com>
2020-05-02 17:24:14 +0200

    Remove zoom2 for soggettario

--- src/trainingData/soggettario.js
+++ src/trainingData/soggettario.js
@@ -1,23 +1,57 @@
 const zoom = require('node-zoom2')
+const { db } = require('../database/sqlite')
 
 const getSoggetto = (isbn) => {
   return new Promise((resolve, reject) => {
-    zoom.connection('193.206.221.29:3950/nopac')
+    zoom.connection('193.206.221.29:2100/nopac')
       .set('preferredRecordSyntax', 'UNIMARC')
       .query('prefix', '@attr 1=7 ' + isbn.replace('-', ''))
-      .createReadStream()
-      .on('data', function (record) {
-        const elem = record.json.fields
-          .filter(elem => Object.keys(elem)[0] === '606')
-          .map(elem => elem['606'] ? elem['606'].subfields : {})
-          .filter(field => Object.keys(field)[0] === 'a')
-          .map(field => field.a)
-        resolve(elem)
-      })
-      .on('close', function (close) {
-        resolve()
+      .search((err, resultset) => {
+        console.log(resultset)
+        if (err || !resultset) {
+          reject(err)
+        }
+        if (resultset) {
+          resultset.getRecords(0, 1, function (err, records) {
+            if (err) {
+              reject(err)
+            }
+            if (records) {
+              const map = []
+              while (records.hasNext()) {
+                const record = records.next()
+                if (record._record) {
+                  map.push(record)
+                }
+              }
+
+              resolve(map[0])
+            }
+          })
+        } else {
+          reject(Error(''))
+        }
       })
   })
 }
 
-module.exports = getSoggetto
+db.serialize(() => {
+  db.all('SELECT isbn FROM TrainingData WHERE isbn IS NOT NULL', async (err, all) => {
+    if (err) {
+      console.error(err)
+    }
+
+    const isbnList = all.filter(row => row && row.isbn).map(row => row.isbn)
+    for (const index in isbnList) {
+      const isbn = isbnList[index]
+      try {
+        const soggetto = await getSoggetto(isbn)
+        if (soggetto && soggetto.length) {
+          console.log(soggetto)
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  })
+})
