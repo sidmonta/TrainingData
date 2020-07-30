c6c4eb6612ac8d03fe34169a45e8f336433238ba
sidmonta <sidmonta.15@gmail.com>
2020-07-30 23:05:33 +0200

    change sql

--- src/trainingData/appendDescriptions.js
+++ src/trainingData/appendDescriptions.js
@@ -1,35 +1,36 @@
 const fetch = require('node-fetch')
 const { db } = require('../database/sqlite')
 
-db.serialize(async () => {
-  db.all('SELECT id, metadata, isbn FROM TrainingData WHERE isbn IS NOT NULL AND desription IS NOT NULL ORDER BY isbn', [], async (err, rows) => {
-    if (err) {
-      console.error(err)
-    }
-    for (const index in rows) {
-      const book = rows[index]
-      const isbn = book.isbn.replace(/-/g, '')
-      console.log(isbn)
-      try {
-        const result = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`, {
-          headers:
-            { Accept: 'application/json' }
-        }).then(data => data.json())
+db.all('SELECT id, metadata, isbn FROM TrainingData WHERE isbn IS NOT NULL AND description IS NULL', [], async (err, rows) => {
+  if (err) {
+    console.error(err)
+  }
+  for (const index in rows) {
+    const book = rows[index]
+    const isbn = book.isbn.replace(/-/g, '')
+    console.log(isbn)
+    try {
+      const result = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`, {
+        headers:
+          { Accept: 'application/json' }
+      }).then(data => data.json())
 
-        if (result.totalItems) {
-          const descr = result.items.reduce((desc, item) =>
-            desc + item.volumeInfo.description + '\n', '')
-          if (descr) {
+      if (result.totalItems) {
+        const descr = result.items.reduce((desc, item) =>
+          desc + item.volumeInfo.description + '\n', '')
+        if (descr) {
+          try {
             db.run('UPDATE TrainingData SET description = ? WHERE id = ?', descr.trim(), book.id)
             console.log(descr)
+          } catch (err) {
+            console.log(err)
           }
         }
-      } catch (err) {
-        console.error(err)
       }
-      await new Promise(resolve => setTimeout(resolve, 2000))
+    } catch (err) {
+      console.error(err)
     }
-
-    db.close()
-  })
+    await new Promise(resolve => setTimeout(resolve, 2000))
+  }
+  db.close()
 })
